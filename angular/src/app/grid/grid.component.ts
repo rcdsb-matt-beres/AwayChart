13488cefce93fc5061a40d80de6f4587a19fd6c0
Matt Beres <203850019+rcdsb-matt-beres@users.noreply.github.com>
2025-05-12 09:16:35 -0400

    remove case sensitivity

--- angular/src/app/grid/grid.component.ts
+++ angular/src/app/grid/grid.component.ts
@@ -83,106 +83,154 @@ export class GridComponent implements OnInit {
 
       if (shouldDisplay) {
         //Remote
-        if (event.summary.split("-")[1].trim() == "Remote") {
-          this.remoteMap.set(event.summary.split("-")[0].trim(), "Remote")
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) != "Off" && !this.displayMap.get(event.summary.split("-")[0].trim())?.includes("Field") && this.displayMap.get(event.summary.split("-")[0].trim()) != "Out of Office" && this.displayMap.get(event.summary.split("-")[0].trim()) != "Conference") {
-            this.displayMap.set(event.summary.split("-")[0].trim(), "Remote")
+        if (event.summary.split("-")[1].trim().toUpperCase() == "REMOTE") {
+          this.remoteMap.set(event.summary.split("-")[0].trim(), "Remote");
+          if (
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "OFF" &&
+            !this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase().includes("FIELD") &&
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "OUT OF OFFICE" &&
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "CONFERENCE"
+          ) {
+            this.displayMap.set(event.summary.split("-")[0].trim(), "Remote");
           }
-        } //Account for Corey's hyphenated last name
-        else if (event.summary.split("-")[0].trim() == "Corey Graveline" && event.summary.split("-")[2].trim() == "Remote") {
-          this.remoteMap.set("Corey Graveline-Dumouchel", "Remote")
-          if (this.displayMap.get("Corey Graveline-Dumouchel") != "Off" && !this.displayMap.get("Corey Graveline-Dumouchel")?.includes("Field") && this.displayMap.get("Corey Graveline-Dumouchel") != "Out of Office" && this.displayMap.get("Corey Graveline-Dumouchel") != "Conference") {
-            this.displayMap.set("Corey Graveline-Dumouchel", "Remote")
+        } else if (
+          event.summary.split("-")[0].trim().toUpperCase() == "COREY GRAVELINE" &&
+          event.summary.split("-")[2].trim().toUpperCase() == "REMOTE"
+        ) {
+          this.remoteMap.set("Corey Graveline-Dumouchel", "Remote");
+          if (
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "OFF" &&
+            !this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase().includes("FIELD") &&
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "OUT OF OFFICE" &&
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "CONFERENCE"
+          ) {
+            this.displayMap.set("Corey Graveline-Dumouchel", "Remote");
           }
         }
 
         //Conference
-        if (event.summary.split("-")[1].trim() == "Conference") {
+        if (event.summary.split("-")[1].trim().toUpperCase() == "CONFERENCE") {
           this.conferenceMap.set(event.summary.split("-")[0].trim(), "Conference");
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) == "Remote") {
+          if (this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "REMOTE") {
             this.displayMap.delete(event.summary.split("-")[0].trim());
           }
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) != "Off" && !this.displayMap.get(event.summary.split("-")[0].trim())?.includes("Field") && this.displayMap.get(event.summary.split("-")[0].trim()) != "Out of Office") {
+          if (
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "OFF" &&
+            !this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase().includes("FIELD") &&
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "OUT OF OFFICE"
+          ) {
             this.displayMap.set(event.summary.split("-")[0].trim(), "Conference");
           }
-        } //Account for Corey's hyphenated last name
-        else if (event.summary.split("-")[0].trim() == "Corey Graveline" && event.summary.split("-")[2].trim() == "Conference") {
+        } else if (
+          event.summary.split("-")[0].trim().toUpperCase() == "COREY GRAVELINE" &&
+          event.summary.split("-")[2].trim().toUpperCase() == "CONFERENCE"
+        ) {
           this.conferenceMap.set("Corey Graveline-Dumouchel", "Conference");
-          if (this.displayMap.get("Corey Graveline-Dumouchel") == "Remote") {
+          if (this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "REMOTE") {
             this.displayMap.delete("Corey Graveline-Dumouchel");
           }
-          if (this.displayMap.get("Corey Graveline-Dumouchel") != "Off" && !this.displayMap.get("Corey Graveline-Dumouchel")?.includes("Field") && this.displayMap.get("Corey Graveline-Dumouchel") != "Out of Office") {
+          if (
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "OFF" &&
+            !this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase().includes("FIELD") &&
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "OUT OF OFFICE"
+          ) {
             this.displayMap.set("Corey Graveline-Dumouchel", "Conference");
           }
         }
 
         //Out of Office
-        if (event.summary.split("-")[1].trim() == "Out of Office") {
+        if (event.summary.split("-")[1].trim().toUpperCase() == "OUT OF OFFICE") {
           this.awayMap.set(event.summary.split("-")[0].trim(), "Out of Office");
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) == "Remote" || this.displayMap.get(event.summary.split("-")[0].trim()) == "Conference") {
+          if (
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "REMOTE" ||
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "CONFERENCE"
+          ) {
             this.displayMap.delete(event.summary.split("-")[0].trim());
           }
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) != "Off" && !this.displayMap.get(event.summary.split("-")[0].trim())?.includes("Field")) {
+          if (
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "OFF" &&
+            !this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase().includes("FIELD")
+          ) {
             this.displayMap.set(event.summary.split("-")[0].trim(), "Out of Office");
           }
-        } //Account for Corey's hyphenated last name
-        else if (event.summary.split("-")[0].trim() == "Corey Graveline" && event.summary.split("-")[2].trim() == "Out of Office") {
-          this.awayMap.set("Corey Graveline-Dumouchel", "Out of Office")
-          if (this.displayMap.get("Corey Graveline-Dumouchel") == "Remote" || this.displayMap.get("Corey Graveline-Dumouchel") == "Conference") {
+        } else if (
+          event.summary.split("-")[0].trim().toUpperCase() == "COREY GRAVELINE" &&
+          event.summary.split("-")[2].trim().toUpperCase() == "OUT OF OFFICE"
+        ) {
+          this.awayMap.set("Corey Graveline-Dumouchel", "Out of Office");
+          if (
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "REMOTE" ||
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "CONFERENCE"
+          ) {
             this.displayMap.delete("Corey Graveline-Dumouchel");
           }
-          if (this.displayMap.get("Corey Graveline-Dumouchel") != "Off" && !this.displayMap.get("Corey Graveline-Dumouchel")?.includes("Field")) {
-            this.displayMap.set("Corey Graveline-Dumouchel", "Out of Office")
+          if (
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "OFF" &&
+            !this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase().includes("FIELD")
+          ) {
+            this.displayMap.set("Corey Graveline-Dumouchel", "Out of Office");
           }
         }
 
         //Field
-        if (event.summary.split("-")[1].trim().includes("Field")) {
+        if (event.summary.split("-")[1].trim().toUpperCase().includes("FIELD")) {
           this.fieldMap.set(event.summary.split("-")[0].trim(), "Field");
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) == "Remote" || this.displayMap.get(event.summary.split("-")[0].trim()) == "Conference" || this.displayMap.get(event.summary.split("-")[0].trim()) == "Out of Office") {
+          if (
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "REMOTE" ||
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "CONFERENCE" ||
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "OUT OF OFFICE"
+          ) {
             this.displayMap.delete(event.summary.split("-")[0].trim());
           }
-          if (this.displayMap.get(event.summary.split("-")[0].trim()) != "Off") {
+          if (this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() != "OFF") {
             if (event.summary.split("(")[1] != null) {
-              this.displayMap.set(event.summary.split("-")[0].trim(), "Field (" + event.summary.split("(")[1].trim());
+              this.displayMap.set(
+                event.summary.split("-")[0].trim(),
+                "Field (" + event.summary.split("(")[1].trim()
+              );
             } else {
-              this.displayMap.set(event.summary.split("-")[0].trim(), "Field")
+              this.displayMap.set(event.summary.split("-")[0].trim(), "Field");
             }
           }
-        } //Account for Corey's hyphenated last name
-        else if (event.summary.split("-")[0].trim() == "Corey Graveline" && event.summary.split("-")[2].trim().includes("Field")) {
-          this.fieldMap.set("Corey Graveline-Dumouchel", "Field")
-          if (this.displayMap.get("Corey Graveline-Dumouchel") == "Remote" || this.displayMap.get("Corey Graveline-Dumouchel") == "Conference" || this.displayMap.get("Corey Graveline-Dumouchel") == "Out of Office") {
+        } else if (
+          event.summary.split("-")[0].trim().toUpperCase() == "COREY GRAVELINE" &&
+          event.summary.split("-")[2].trim().toUpperCase().includes("FIELD")
+        ) {
+          this.fieldMap.set("Corey Graveline-Dumouchel", "Field");
+          if (
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "REMOTE" ||
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "CONFERENCE" ||
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "OUT OF OFFICE"
+          ) {
             this.displayMap.delete("Corey Graveline-Dumouchel");
           }
-          if (this.displayMap.get("Corey Graveline-Dumouchel") != "Off") {
-            this.displayMap.set("Corey Graveline-Dumouchel", "Field")
+          if (this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() != "OFF") {
+            this.displayMap.set("Corey Graveline-Dumouchel", "Field");
           }
         }
 
         //Out
-        if (event.summary.split("-")[1].trim() == "Off") {
+        if (event.summary.split("-")[1].trim().toUpperCase() == "OFF") {
           this.offMap.set(event.summary.split("-")[0].trim(), "Off");
           if (
-            this.displayMap.get(event.summary.split("-")[0].trim()) == "Remote" ||
-            this.displayMap.get(event.summary.split("-")[0].trim()) == "Conference" ||
-            this.displayMap.get(event.summary.split("-")[0].trim()) == "Out of Office" ||
-            this.displayMap.get(event.summary.split("-")[0].trim())?.includes("Field")
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "REMOTE" ||
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "CONFERENCE" ||
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase() == "OUT OF OFFICE" ||
+            this.displayMap.get(event.summary.split("-")[0].trim())?.toUpperCase().includes("FIELD")
           ) {
             this.displayMap.delete(event.summary.split("-")[0].trim());
           }
           this.displayMap.set(event.summary.split("-")[0].trim(), "Off");
-        } //Account for Corey's hyphenated last name
-        else if (
-          event.summary.split("-")[0].trim() == "Corey Graveline" &&
-          event.summary.split("-")[2].trim() == "Off"
+        } else if (
+          event.summary.split("-")[0].trim().toUpperCase() == "COREY GRAVELINE" &&
+          event.summary.split("-")[2].trim().toUpperCase() == "OFF"
         ) {
           this.offMap.set("Corey Graveline-Dumouchel", "Off");
           if (
-            this.displayMap.get("Corey Graveline-Dumouchel") == "Remote" ||
-            this.displayMap.get("Corey Graveline-Dumouchel") == "Conference" ||
-            this.displayMap.get(event.summary.split("-")[0].trim()) == "Out of Office" ||
-            this.displayMap.get(event.summary.split("-")[0].trim())?.includes("Field")
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "REMOTE" ||
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "CONFERENCE" ||
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase() == "OUT OF OFFICE" ||
+            this.displayMap.get("Corey Graveline-Dumouchel")?.toUpperCase().includes("FIELD")
           ) {
             this.displayMap.delete("Corey Graveline-Dumouchel");
           }
