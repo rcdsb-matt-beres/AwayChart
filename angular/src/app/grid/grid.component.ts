89674f2e69887451b62fe3d7a552d095d6249533
Matt Beres <203850019+rcdsb-matt-beres@users.noreply.github.com>
2025-12-12 14:39:15 -0500

    add ben

--- angular/src/app/grid/grid.component.ts
+++ angular/src/app/grid/grid.component.ts
@@ -23,6 +23,7 @@ export class GridComponent implements OnInit {
     "Andrew Lebert",
     "Andrew Smith",
     "Anthony Walsh",
+    "Ben Robinson",
     "Bethany McCulloch",
     "Corey Graveline Dumouchel",
     "Chris Waito",
@@ -42,14 +43,14 @@ export class GridComponent implements OnInit {
     "Terri Lyn Lee",
     "Trevor Smith",
     "Tyler Lloyd Gallan",
-    "" // This is here for when theres an odd number of names
+    // "" // This is here for when theres an odd number of names
   ];
 
   events: any[] = [];
   displayMap = new Map<String, String>();
 
   ngOnInit(): void {
-    // this.names.sort(); // This is here for when theres an even number of names, it just makes sure it's alphabetical
+    this.names.sort(); // This is here for when theres an even number of names, it just makes sure it's alphabetical
 
     // Grab events from both calendars on load
     this.calendarService.getEvents()
